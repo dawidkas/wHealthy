c19f6fbc75d3080f7af9907dd4ef94e4594c2538
Dawid Kasprzyk <dawid.kasprzyk@wp.pl>
2021-07-21 00:33:53 +0200

    basic SecurityConfig.java added

--- src/main/java/pl/coderslab/SecurityConfig.java
+++ src/main/java/pl/coderslab/SecurityConfig.java
@@ -1,10 +1,21 @@
 package pl.coderslab;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/about").authenticated()
+                .antMatchers("/about/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and().formLogin();
+    }
 }
